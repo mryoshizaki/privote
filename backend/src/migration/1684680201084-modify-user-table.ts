6165f441963f78e99eefbd53433a879f588ccf29
Ubuntu <ubuntu@ubuntu-2204.linuxvmimages.local>
2023-05-25 00:35:48 -0400

    modified migrations

--- backend/src/migration/1684680201084-modify-user-table.ts
+++ backend/src/migration/1684680201084-modify-user-table.ts
@@ -6,8 +6,6 @@ export class modifyUserTable1684680042496 implements MigrationInterface {
     name = 'modifyUserTable1684680042496'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`user\` DROP COLUMN \`name\``);
-        await queryRunner.query(`ALTER TABLE \`user\` ADD \`verified\` tinyint NOT NULL DEFAULT 0`);
         await queryRunner.query(`ALTER TABLE \`user\` ADD \`first_name\` varchar(255) NOT NULL`);
         await queryRunner.query(`ALTER TABLE \`user\` ADD \`last_name\` varchar(255) NOT NULL`);
         await queryRunner.query(`ALTER TABLE \`user\` ADD \`sex\` enum ('Male', 'Female') NOT NULL`);
@@ -27,8 +25,6 @@ export class modifyUserTable1684680042496 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE \`user\` DROP COLUMN \`sex\``);
         await queryRunner.query(`ALTER TABLE \`user\` DROP COLUMN \`last_name\``);
         await queryRunner.query(`ALTER TABLE \`user\` DROP COLUMN \`first_name\``);
-        await queryRunner.query(`ALTER TABLE \`user\` DROP COLUMN \`verified\``);
-        await queryRunner.query(`ALTER TABLE \`user\` ADD \`name\` varchar(100) NOT NULL`);
     }
 
 }
